4106eab15e7ee833e76c12aaf9e396de7a7996df
Sakerini <sakerini1999@gmail.com>
2020-05-07 11:35:43 +0300

    Rest API to rest Client connected

---
+++ crm-web-app-rest-client-demo/src/main/java/com/sakerini/demo/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.sakerini.demo.service;
+
+import java.util.List;
+
+import com.sakerini.demo.model.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
